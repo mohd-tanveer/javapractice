9ef0f6618f347cb5903d703fc43ca374a1f2b267
Mohd Tanveer Shaikh <35370937+mohd-tanveer@users.noreply.github.com>
2020-07-20 22:19:32 +0530

    Update Interface.java
    default method in interface java8 feature

--- Interface.java
+++ Interface.java
@@ -23,3 +23,15 @@ public class Interface{
 	obj.show();
 }
 }
+
+
+/*
+
+//default method in interface
+interface Demo{
+	void abc();
+	default void show(){
+		sop('print default mehtod is allowd in java 8 ')
+		//since implementation is not allowed in inter face so it will throw an error if we don't use Default keyword
+	}
+}*/
