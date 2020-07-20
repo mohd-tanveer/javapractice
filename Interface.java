f615130e73c5dba759d9bbfd2e639db6515efea0
Mohd Tanveer Shaikh <35370937+mohd-tanveer@users.noreply.github.com>
2020-07-20 22:28:03 +0530

    Update Interface.java
    default method,static method  in interface by default any variable will of final

--- Interface.java
+++ Interface.java
@@ -34,4 +34,21 @@ interface Demo{
 		sop('print default mehtod is allowd in java 8 ')
 		//since implementation is not allowed in inter face so it will throw an error if we don't use Default keyword
 	}
-}*/
+	
+but this require another class which will imp this
+how ever we have static method which can be used and in main method we can direclty call by interfacename.staticmethodname
+
+
+interface Demo{
+	void abc();
+	static void show(){
+		sop('print default mehtod is allowd in java 8 ')
+		//since implementation is not allowed in inter face so it will throw an error if we don't use Default keyword
+	}
+}
+
+demo.show()
+
+any variable is Final by default in interface}
+
+*/
